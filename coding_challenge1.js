f09c2ce390451253059429bcb5795b6d3dd3357e
Chadpaul97 <chadpaul97@gmail.com>
2022-01-04 19:59:49 -0600

    Logical Operators & And statement added

--- coding_challenge1.js
+++ coding_challenge1.js
@@ -1,3 +1,5 @@
+
+//Coding Challenge 1
 const heightJohn = 1.95
 const massJohn = 92
 
@@ -10,3 +12,43 @@ const markHigherBMI = BMIMARK > BMIJOHN
 console.log(BMIJOHN)
 console.log(BMIMARK)
 console.log(markHigherBMI)
+
+//Coding Challenge 2
+if (BMIJOHN < BMIMARK){
+    console.log("Mark's BMI is higher than John's")
+    console.log("Mark's BMI ", BMIMARK)
+    console.log("John's BMI ", BMIJOHN)
+}else{
+    console.log("John's BMI is higher than Mark's")
+    console.log("John's BMI ", BMIJOHN)
+    console.log("Mark's BMI ", BMIMARK)
+}
+
+//Coding Challenge 3
+
+const dolphinsScore = [96 + 108 + 89] /3;
+const koalasScore = [88 + 91 + 110] /3 ; 
+console.log(dolphinsScore)
+console.log(koalasScore)
+if( dolphinsScore < koalasScore){
+    console.log("Koalas Win by " + (koalasScore - dolphinsScore  ))
+}else{
+    console.log("Dolphins Win by " + (dolphinsScore - koalasScore))
+}
+
+const dolphinsScore1 = [97 + 112 + 101] /3;
+const koalasScore1 = [109 + 95 + 123] /3;
+console.log(dolphinsScore1);
+console.log(koalasScore1);
+
+if (dolphinsScore1 < koalasScore1 && koalasScore1 >= 100){
+    console.log("Koalas Win");
+} else if(koalasScore1 < dolphinsScore1 && dolphinsScore1 >= 100){
+console.log("Dolphins Win");
+} else if(dolphinsScore1 === koalasScore1 && dolphinsScore1 >= 100 && koalasScore1 >= 100){
+console.log("Both win!!");
+}else{
+    console.log("No one Wins ☹")
+}
+
+//Coding Challenge 4
--- fundamentals.js
+++ fundamentals.js
@@ -28,13 +28,13 @@ language = "Spanish";
 
 // LECTURE: Basic Operators
 
-console.log(population/2)
+console.log(population / 2)
 population++;
 console.log(population);
 console.log(population > 6);
 console.log(population < 33)
 
-const description1 = country +' is in ' +continent +', and its ' +population +' million people speak ' +language;
+const description1 = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 console.log(description1)
 
 
@@ -44,20 +44,36 @@ const description = `${country} is in ${continent}, and its ${population} millio
 console.log(description);
 
 // LECTURE: Taking Decisions: if / else Statements
-if(population > 33){
+if (population > 33) {
     console.log(`${country} is above average`)
-}else{
+} else {
     console.log(`${country} is ${33 - population} millon below average`)
 }
 
 // LECTURE: Type Conversion and Coercion
-console.log('9'-'5');
-console.log('19'-'13'+'17');
-console.log('19'-'13'+ 17);
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
 console.log('123' < 57);
-console.log(5 + 6 + '4' + 9 -4 - 2);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
 
 // LECTURE: Equality Operators: == vs. ===
 
-let numNeighbours = 'How many neighbour countries does your countryhave?';
+let numNeighbours = prompt('How many neighbour countries does your countryhave?')
+// let numNeighbours = 1
+// if (numNeighbours === 1){
+if (numNeighbours == 1) {
+    console.log('Only 1 border!')
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border in case')
+} else {
+    console.log('No borders')
+}
 
+//LECTURE: Logical Operators
+
+if (language === 'english' && population < 50 && !isIsland) {
+    console.log(`You should live in ${country} :)`);
+} else {
+    console.log(`${country} does not meet your criteria :(`);
+}
